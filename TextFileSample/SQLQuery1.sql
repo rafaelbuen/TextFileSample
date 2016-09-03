89842c9d2183a966e75f799417c5ea7fc30f8300
rafaelbuen <rafaelbuen@hotmail.com>
2016-09-03 14:00:43 -0700

    checkin

---
+++ TextFileSample/SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿
+drop table [dbo].[FilesImport]
+go
+drop table [dbo].[FilesDataRaw]
+go
+select * from [dbo].[FilesImport]
+go
+select * from [dbo].[FilesDataRaw] 
+go
+
+/*
+insert into [dbo].[FilesImport] (FilePath, DateCreated, FileStatus) 
+values (?, getdate(), 'FileNameRead')
+*/
---
+++ TextFileSample/SqlObjects.sql
@@ -0,0 +1,26 @@
+﻿
+
+if not exists(select table_name from information_schema.tables where table_name = 'FilesImport') 
+begin 
+	create table dbo.FilesImport 
+		( ID bigint identity primary key clustered 
+		, FilePath varchar(1000) 
+		, DateCreated datetime 
+		, FileStatus varchar(50)
+		)
+end 
+
+
+if not exists(select table_name from information_schema.tables where table_name = 'FilesDataRaw') 
+begin 
+	create table dbo.FilesDataRaw 
+		( ID bigint
+		, FilesImportID int
+		, Content varchar(max)
+		, Result varchar(50)
+		)
+end 
+
+--select * from dbo.FilesImport 
+
+--select * from dbo.FilesDataRaw
